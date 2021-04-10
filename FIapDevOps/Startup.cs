b1e1696485621146f766a4f287d16a64b1d693e7
Diego Marlyson Santos Duque <marlyson.cc@hotmail.com>
2021-04-10 00:13:16 -0300

    Cu

--- FIapDevOps/Startup.cs
+++ FIapDevOps/Startup.cs
@@ -51,7 +51,7 @@ namespace FIapDevOps
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=TBALUNO}/{action=Index}/{id?}");
             });
         }
     }
